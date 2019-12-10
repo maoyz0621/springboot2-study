2652ea23e83df61be0d43229098f69df52507a4b
“maoyz0621” <245890416@qq.com>
2019-12-10 23:46:05 +0800

    swagger

---
+++ swagger-springboot2/src/main/java/com/myz/swagger/SwaggerApplication.java
@@ -0,0 +1,15 @@
+package com.myz.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class SwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerApplication.class, args);
+    }
+}
